626a78298fe85429b8cbe1289ec81c33d9ee3b00
Roberto <anacleto44@gmail.com>
2021-09-28 08:11:01 +0200

    defaultWorker javadoc update

--- src/defaultWorker.java
+++ src/defaultWorker.java
@@ -2,12 +2,21 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 
+/**
+ * default worker extending default job
+ * it should be extended doing nothing
+ * @param <T> Custom type of the channels
+ */
 public class defaultWorker<T> extends defaultJob<T> {
     public defaultWorker(int id, T EOF) {
         this.id = id;
         this.EOF = EOF;
     }
 
+    /**
+     * in this method the custom code should be added overriding this class
+     * see default job for channels and locks name
+     */
     @Override
     public void runJob() {
 
