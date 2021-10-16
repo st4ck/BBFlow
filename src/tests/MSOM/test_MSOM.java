161d578fca03afda304e59c5a3f34471568aa838
Roberto <anacleto44@gmail.com>
2021-10-16 16:26:50 +0200

    uploading working MSOM example

--- src/tests/MSOM/test_MSOM.java
+++ src/tests/MSOM/test_MSOM.java
@@ -7,17 +7,28 @@ import java.util.ArrayList;
 public class test_MSOM {
     public static void main (String[] args) {
         //testMSOM(Integer.parseInt(args[0]));
-        testMSOM(1);
-        testMSOM(2);
-        testMSOM(4);
-        testMSOM(8);
-        testMSOM(16);
+        testMSOM(1024,1);
+        testMSOM(1024,2);
+        testMSOM(1023,3);
+        testMSOM(1024,4);
+        testMSOM(1025,5);
+        testMSOM(1026,6);
+        testMSOM(1029,7);
+        testMSOM(1024,8);
+        testMSOM(1026,9);
+        testMSOM(1030,10);
+        testMSOM(1023,11);
+        testMSOM(1020,12);
+        testMSOM(1027,13);
+        testMSOM(1022,14);
+        testMSOM(1020,15);
+        testMSOM(1024,16);
     }
 
-    private static void testMSOM(int split) {
+    private static void testMSOM(int size, int split) {
         int depth = 3;
         int searchMaxThreads = 8;
-        MSOM z = new MSOM(1024,depth,split);
+        MSOM z = new MSOM(size,depth,split);
         z.start();
         customWatch myWatch = new customWatch();
         myWatch.start();
@@ -29,7 +40,7 @@ public class test_MSOM {
         res.bestj = 0;*/
         //System.out.println("Best position found is in matrix "+res.i+","+res.j+" in position "+res.besti+","+res.bestj);
         //System.out.println("Training vector 100 times");
-        for (int i=0; i<100; i++) {
+        for (int i=0; i<10; i++) {
             ArrayList<Double> vector = new ArrayList<>();
             for (int j=0; j<depth; j++) {
                 vector.add(Math.random()*255);
