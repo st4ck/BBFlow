769dcccfd4bae36e3cad633430317eb24770e7b2
Roberto <anacleto44@gmail.com>
2021-11-09 17:37:54 +0100

    updating benchmarks

--- src/tests/benchmarks/distributed/benchmark_network_farm.java
+++ src/tests/benchmarks/distributed/benchmark_network_farm.java
@@ -11,10 +11,19 @@ public class benchmark_network_farm {
         preloader.preloadJVM();
 
         int n = 1000;
+        int start_nodes = -1;
+        String host = "127.0.0.1";
         if (args.length > 0) {
             n = Integer.parseInt(args[0]);
-            if (args.length == 2) {
-                objectClient.flushThreshold = Long.parseLong(args[1]);
+            if (args.length > 1) {
+                start_nodes = Integer.parseInt(args[1]); // node to start, [0,1]. 0=E+C, 1=Ws, -1 for all
+                if (args.length > 2) {
+                    host = args[2]; // host for the client node
+                    if (args.length > 3) {
+                        objectClient.flushThreshold = Long.parseLong(args[1]);
+                    }
+                }
+
             }
         }
         int finalN = n;
@@ -39,22 +48,35 @@ public class benchmark_network_farm {
             }
         };
 
-        String host = "127.0.0.1";
+        boolean startEC = false;
+        boolean startW = false;
+
+        if ((start_nodes == -1) || (start_nodes == 0)) startEC = true;
+        if ((start_nodes == -1) || (start_nodes == 1)) startW = true;
+
         int n_workers = 3;
+        if (!startW) { n_workers = 0; }
 
         ff_farm farm = new ff_farm<Long,Long>(n_workers, Worker);
         farm.removeEmitter();
         farm.removeCollector();
-        farm.emitter = new ff_node(Emitter);
-        farm.collector = new ff_node(new defaultCollector<Long>());
-        farm.collector.addOutputChannel(new ff_queue());
+        if (startEC) {
+            farm.emitter = new ff_node(Emitter);
+            farm.collector = new ff_node(new defaultCollector<Long>());
+            farm.collector.addOutputChannel(new ff_queue());
+        }
 
         // connect emitter, workers and collector with network channels
         for (int i=0; i<n_workers; i++) {
-            farm.emitter.addOutputChannel(new ff_queue_TCP(ff_queue_TCP.OUTPUT,i, host));
-            ((ff_node<Long,Long>)farm.workers.get(i)).addInputChannel(new ff_queue_TCP(ff_queue_TCP.INPUT, i));
-            ((ff_node<Long,Long>)farm.workers.get(i)).addOutputChannel(new ff_queue_TCP(ff_queue_TCP.OUTPUT,i+n_workers, host));
-            farm.collector.addInputChannel(new ff_queue_TCP(ff_queue_TCP.INPUT, i+n_workers));
+            if (startEC) {
+                farm.emitter.addOutputChannel(new ff_queue_TCP(ff_queue_TCP.OUTPUT, i, host));
+                farm.collector.addInputChannel(new ff_queue_TCP(ff_queue_TCP.INPUT, i + n_workers));
+            }
+
+            if (startW) {
+                ((ff_node<Long, Long>) farm.workers.get(i)).addInputChannel(new ff_queue_TCP(ff_queue_TCP.INPUT, i));
+                ((ff_node<Long, Long>) farm.workers.get(i)).addOutputChannel(new ff_queue_TCP(ff_queue_TCP.OUTPUT, i + n_workers, host));
+            }
         }
 
         customWatch w = new customWatch();
